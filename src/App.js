64f02dbe2114f5408d61a5d757624cc0d6d8d58a
katherine95 <kathiekim95@gmail.com>
2019-05-19 18:39:16 +0300

    add comments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { createStore } from "redux";
 import reducer from "./reducers";
 
 const initialState = { tech : "Redux" };
+/* create a redux store using createStore function
+   pass in the reducer and initialState as an argument */
 const store = createStore(reducer, initialState);
 
 class App extends Component {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+/* create a reducer function that takes in two parameters(state and action */
 export default (state) => {
     return state
 }
